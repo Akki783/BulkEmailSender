2c7db6db97a83c775ce6ee094467b928f34bcead
Akki783 <platformsupport@dove-soft.com>
2024-10-24 14:50:42 +0530

    update: testing

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const EmailRecord = require("./Model/HR1847REC");
 const db_connect = require("./config/DB_CONFIG");
 
 
-// Connect to the database
 db_connect();
 
 async function sendEmails() {
     try {
         console.log('Starting email sending process...');
 
-        const batchSize = 200; // Change back to 500 for real use
+        const batchSize = 10;
         const sentEmailsCount = await EmailRecord.countDocuments({ sent: true });
         console.log(`Sent emails count: ${sentEmailsCount}`);
 
@@ -27,22 +26,13 @@ async function sendEmails() {
             return;
         }
 
-
-       
         for (const record of records) {
             console.log("Email Id:", record.Email);
             const mailOptions = {
                 from: 'khushdesai1030@gmail.com',
-                to: record.Email, // You may want to change this to record.Email
-                subject: 'Inquiry for Full Stack Developer Opportunities',
+                to: 'lesbmao@gmail.com', // You may want to change this to record.Email
+                subject: 'Testing',
                 text: `
-Dear ${record.Name},\n
-I hope this message finds you well. My name is Khush Desai, and I am writing to inquire about full stack developer opportunities at ${record.Company}. I have developed skills in both front-end and back-end technologies, and I am eager to contribute to your team.\n
-Please find my CV attached for your reference. I would appreciate the chance to discuss how I can add value to your projects and organization.\n
-Thank you for your time and consideration. I look forward to hearing from you.\n
-Best regards,\n
-Khush Desai\n
-+91 9909048412
 `,
                 attachments: [
                     {
@@ -62,13 +52,47 @@ Khush Desai\n
         }
         
 
+       
+//         for (const record of records) {
+//             console.log("Email Id:", record.Email);
+//             const mailOptions = {
+//                 from: 'khushdesai1030@gmail.com',
+//                 to: record.Email, // You may want to change this to record.Email
+//                 subject: 'Inquiry for Full Stack Developer Opportunities',
+//                 text: `
+// Dear ${record.Name},\n
+// I hope this message finds you well. My name is Khush Desai, and I am writing to inquire about full stack developer opportunities at ${record.Company}. I have developed skills in both front-end and back-end technologies, and I am eager to contribute to your team.\n
+// Please find my CV attached for your reference. I would appreciate the chance to discuss how I can add value to your projects and organization.\n
+// Thank you for your time and consideration. I look forward to hearing from you.\n
+// Best regards,\n
+// Khush Desai\n
+// +91 9909048412
+// `,
+//                 attachments: [
+//                     {
+//                         filename: 'KHUSH_CV.pdf', 
+//                         path: './KHUSH_CV.pdf'  
+//                     }
+//                 ]
+//             };
+        
+//             // Send email
+//             try {
+//                 let info = await transporter.sendMail(mailOptions);
+//                 console.log('Email sent: ' + info.response);
+//             } catch (error) {
+//                 console.error('Error sending email to:', record.Email, error);
+//             }
+//         }
+        
 
-        // Update the records as sent
-        const ids = records.map(record => record._id);
-        await EmailRecord.updateMany(
-            { _id: { $in: ids } },
-            { $set: { sent: true } }
-        );
+
+        // // Update the records as sent
+        // const ids = records.map(record => record._id);
+        // await EmailRecord.updateMany(
+        //     { _id: { $in: ids } },
+        //     { $set: { sent: true } }
+        // );
 
         console.log('Records updated as sent.');
 
@@ -77,8 +101,6 @@ Khush Desai\n
     }
 }
 
-// Schedule the job to run once every 24 hours
-// Uncomment the following line to enable scheduling
 const now = new Date();
 const runTime = new Date(now.getTime() + 2 * 60000);
 
@@ -87,5 +109,5 @@ schedule.scheduleJob(runTime, () => {
     sendEmails();
 });
 
-// Run immediately for testing
+
 sendEmails();
