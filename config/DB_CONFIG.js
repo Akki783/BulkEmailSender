8bea19419f1f210c8525e34124843c33e2b3364b
Akki783 <platformsupport@dove-soft.com>
2024-10-24 14:25:50 +0530

    Update : Automate Email Feature

---
+++ config/DB_CONFIG.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+const URL = process.env.MONGO_URL;
+
+// Connect to MongoDB using Mongoose
+const connectDB = async () => {
+    try {
+        await mongoose.connect(URL, {
+            // Removed deprecated options
+            serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+            socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('MongoDB connection error: ', err);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB; // Export the connectDB function
